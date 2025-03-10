58c7ada38be1f41c84be50a1c74c3e68ae34373a
Fateh-Aletomeh <86195398+Fateh-Aletomeh@users.noreply.github.com>
2025-03-10 13:26:10 +0000

    Added file saving functionality. Deleted svg icons.

---
+++ src/main/java/ucl/ac/uk/servlets/SaveFileServlet.java
@@ -0,0 +1,44 @@
+package ucl.ac.uk.servlets;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import ucl.ac.uk.model.Model;
+
+
+@WebServlet("/SaveFileServlet")
+public class SaveFileServlet extends HttpServlet {
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    // Recieve data from webpage
+    String referer = request.getHeader("referer");
+    String fileContent = request.getParameter("noteContent");
+
+    // Parse data
+    String queryString = referer.split("\\?")[1];
+    String[] params = queryString.split("&");
+    String dir = null;
+    String file = null;
+
+    for (String param : params) {
+      String[] keyValue = param.split("=");
+      if (keyValue[0].equals("dir")) {
+        dir = keyValue[1];
+      } else if (keyValue[0].equals("file")) {
+        file = keyValue[1] + ".txt";
+      }
+    }
+
+    // Code to use the model to process something would go here
+    Model notes = new Model();
+    notes.updateFile(dir, file, fileContent);
+
+    // Then return to webpage
+    response.sendRedirect(referer);
+  }
+}
